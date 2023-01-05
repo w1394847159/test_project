d3d06945e2a5b61e0c330994811871a6e69694a5
w1394847159 <1394847159@qq.com>
2023-01-05 09:34:53 +0800

    New1

---
+++ src/main/java/com/wufan/service/New1.java
@@ -0,0 +1,14 @@
+package com.wufan.service;
+
+import java.util.ArrayList;
+
+public class New1 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("hello");
+
+        System.out.println("测试New1");
+    }
+}
